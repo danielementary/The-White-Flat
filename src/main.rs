37114c5d7005d6028de62a366773b57a84442a79
Daniel Filipe Nunes Silva <danielementary@proton.me>
2024-11-18 00:25:39 +0100

    basic db connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
+use sqlx::mysql::MySqlPoolOptions;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, The White Flat!");
+
+    let database_url = format!(
+        "mariadb://{user}:{pass}@{host}:{port}/{database}",
+        user = "root",
+        pass = "my-secret-pw",
+        host = "127.0.0.1",
+        port = "3306",
+        database = "mydatabase"
+    );
+
+    println!("Database URL: {}", database_url);
+
+    let connection = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Failed to connect to database");
+
+    println!("Connected to database");
 }
